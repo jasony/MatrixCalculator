bd40038dfcb3b8d45e931c9bd7f991acc9570dda
Jason Yuen <jyuen.nyc@gmail.com>
2015-03-22 18:20:42 -0400

    Move commands into the ViewModel for proper MVVM

--- MatrixCalculator/MainWindow.xaml.cs
+++ MatrixCalculator/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MatrixCalculator.MatrixOperations;
+using MatrixCalculator.ViewModels;
 
 namespace MatrixCalculator
 {
@@ -21,55 +22,13 @@ namespace MatrixCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainWindowViewModel viewModel = new MainWindowViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private double[,] GetMatrixInputAsDoubleArray()
-        {
-            // process input
-            string inputText = this.MatrixInputTextbox.Text;
-            string[] lines = inputText.Split(new Char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-            var finalDoubleList = new List<List<double>>();
-            foreach (var line in lines)
-            {
-                string[] numbers = line.Split(' ');
-                var doubleList = new List<double>();
-                foreach (var number in numbers)
-                {
-                    doubleList.Add(Convert.ToDouble(number));
-                }
-                finalDoubleList.Add(doubleList);
-            }
-
-            double[,] finalDoubleArray = new double[lines.Count(), lines.Count()];
-            for (int i = 0; i < lines.Count(); i++)
-            {
-                for (int j = 0; j < lines.Count(); j++)
-                {
-                    finalDoubleArray[i, j] = finalDoubleList[i][j];
-                }
-
-            }
-            return finalDoubleArray;
-        }
-
-        private void MatrixInverseButton_Click(object sender, RoutedEventArgs e)
-        {
-
-            this.MatrixOutputTextbox.Text = MatrixOperations.MatrixOperations.CalculateInverse(GetMatrixInputAsDoubleArray()).ToString();
-        }
-
-        private void MatrixDeterminantButton_Click(object sender, RoutedEventArgs e)
-        {
-
-            this.MatrixOutputTextbox.Text = MatrixOperations.MatrixOperations.CalculateDeterminant(GetMatrixInputAsDoubleArray()).ToString();
-        }
-
-        private void MatrixCholeskyButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.MatrixOutputTextbox.Text = MatrixOperations.MatrixOperations.CalculateCholesky(GetMatrixInputAsDoubleArray()).ToString();
+            this.DataContext = viewModel;
         }
     }
 }
